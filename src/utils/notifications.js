264d693785e010a61f9f1f5b3c1feb0f16c54396
mDevlopers <mathias.tss2012@gmail.com>
2026-02-06 19:27:58 +0100

    Update notifications.js

--- src/utils/notifications.js
+++ src/utils/notifications.js
@@ -1,5 +1,18 @@
 function notify(title, body) {
-  new Notification(title, { body });
+  if (Notification.permission === 'granted') {
+    new Notification(title, { body });
+  } else if (Notification.permission !== 'denied') {
+    Notification.requestPermission().then(permission => {
+      if(permission === 'granted') new Notification(title, { body });
+    });
+  }
 }
 
-module.exports = { notify };
+// Exemple : rappel pour note importante
+function scheduleReminder(noteName, delayMs) {
+  setTimeout(() => {
+    notify('Rappel mNotes', `Ta note "${noteName}" doit être consultée !`);
+  }, delayMs);
+}
+
+module.exports = { notify, scheduleReminder };
